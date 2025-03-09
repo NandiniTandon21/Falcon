9aefbdf08518d2f0fd5d14b3f3842f92ffa17b9d
Nandini Tandon <63950748+NandiniTandon21@users.noreply.github.com>
2025-03-09 21:12:02 +0530

    feat/chat

---
+++ falcon/src/components/tag.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+interface TagProps {
+  label: string;
+  icon?: React.ReactNode;
+  selected: boolean;
+  onClick: () => void;
+  className?: string;
+  selectedClassName?: string;
+}
+
+const Tag: React.FC<TagProps> = ({
+  label,
+  icon,
+  selected,
+  onClick,
+  className,
+  selectedClassName,
+}) => {
+  return (
+    <button
+      className={`px-4 py-2 my-1 rounded-full flex items-center gap-2 ${
+        selected
+          ? `${selectedClassName || "bg-blue-500 text-white"}`
+          : "bg-blue-200 text-blue-500 hover:bg-blue-300 hover:text-white"
+      } ${className}`}
+      onClick={onClick}
+    >
+      {icon && <span>{icon}</span>}
+      {label}
+    </button>
+  );
+};
+
+export default Tag;
